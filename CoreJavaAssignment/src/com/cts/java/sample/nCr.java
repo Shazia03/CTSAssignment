9140e5cd408e1fcce9a24c28b042ab603abdf279
unknown <shaziagold3@gmail.com>
2018-05-25 12:48:29 +0530

    nCr program modified

--- CoreJavaAssignment/src/com/cts/java/sample/nCr.java
+++ CoreJavaAssignment/src/com/cts/java/sample/nCr.java
@@ -5,26 +5,15 @@ public class nCr
 {
 	public double nCrnum(int n, int r)
 	{
- 		int rfact=1, nfact=1, nrfact=1,temp1 = n-r ,temp2 = r;
- 		if(r>n-r)
- 		{
- 			temp1 =r;
- 			temp2 =n-r;
- 		}
- 		for(int i=1;i<=n;i++)
- 		{
- 			if(i<=temp2)
- 			{
- 				rfact *= i;
- 				nrfact *= i;
- 			}
- 			else if(i<=temp1)
- 			{
- 				nrfact *= i;
- 			}
- 			nfact *= i;
- 		}
- 		return nfact/(double)(rfact*nrfact);
+		
+		Factorial factobj=new Factorial();
+		int temp=n-r;
+		int a=factobj.factorialnum(n);
+		int b=factobj.factorialnum(temp);
+		int c=factobj.factorialnum(r);
+		
+		return a/(b*c);
+ 		
      }
 
 }
