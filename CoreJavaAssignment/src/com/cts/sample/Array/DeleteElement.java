a4dc5d5dea3c463467e7dce2028179e8625787a0
abbas886 <abbas886@yahoo.com>
2018-05-25 11:40:58 +0530

    added comments.

--- CoreJavaAssignment/src/com/cts/sample/Array/DeleteElement.java
+++ CoreJavaAssignment/src/com/cts/sample/Array/DeleteElement.java
@@ -4,20 +4,23 @@ import java.util.Arrays;
 
 public class DeleteElement
 {
+	//deleting or inserting??
     public String deleteElementpos(int arr[],int pos,int insert)
     {
        int size=arr.length;
        int i;
        size++;
+	    //why size++ and size-1 ??
         for(i=pos-1; i>size-1; i--)
        {
            arr[i] = arr[i-1];
        }
 	   
        arr[pos] = insert;
-	   
+	   //why to convert into string?
 	   return Arrays.toString(arr);
    }
+	//what you are deleting?
     public String deleteElement(String arr[])
     {
     	arr[1]=arr[arr.length-1];
